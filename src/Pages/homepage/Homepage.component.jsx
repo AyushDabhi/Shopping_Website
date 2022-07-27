21ee29f068a434b0832fe453cb175b1fb7bebcf7
Ayush <ayushdabhi31@gmail.com>
2022-07-27 23:33:54 +0530

    Created homepage and broken our homepage into smaller comonents, finished styling our menu-item component

---
+++ src/Pages/homepage/Homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Directory from "../../component/directory/directory.component";
+
+import './Homepage.styles.scss'
+
+const Homepage = () => (
+    <Directory />
+);
+ 
+ 
+
+export default Homepage;
+
